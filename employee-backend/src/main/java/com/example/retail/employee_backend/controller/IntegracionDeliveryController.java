0fcb1d65f862655f6a73fd9f7e5545e9f2d67567
Guorti <Gustavo.ortiz7319@gmail.com>
2025-06-02 23:54:44 -0500

    add cambiar stock

--- employee-backend/src/main/java/com/example/retail/employee_backend/controller/IntegracionDeliveryController.java
+++ employee-backend/src/main/java/com/example/retail/employee_backend/controller/IntegracionDeliveryController.java
@@ -3,6 +3,7 @@ package com.example.retail.employee_backend.controller;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.retail.employee_backend.dto.DeliveryDataDTO;
 import com.example.retail.employee_backend.dto.RetailDataDTO;
 import com.example.retail.employee_backend.model.Producto;
+import com.example.retail.employee_backend.repository.ProductoRepository;
 import com.example.retail.employee_backend.services.IntegracionDeliveryService;
 
 @RestController
@@ -22,18 +24,29 @@ import com.example.retail.employee_backend.services.IntegracionDeliveryService;
 @CrossOrigin(origins = "*")
 public class IntegracionDeliveryController {
 
+    private final ProductoRepository productoRepository;
+
+    @Autowired
+    public IntegracionDeliveryController(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
     @Autowired
     private IntegracionDeliveryService integracionDeliveryService;
 
     @PostMapping("/pedido")
     public ResponseEntity<String> recibirPedido(@RequestBody DeliveryDataDTO deliveryData) {
-        // For now, just print it or log it
         System.out.println("Pedido recibido:");
         System.out.println("ID: " + deliveryData.getId());
         System.out.println("Cédula: " + deliveryData.getCedula());
         System.out.println("Productos: " + deliveryData.getProductos().size());
 
-        // Forward the request to the external endpoint
+        // Obtener el producto y actualizar stock
+        Producto producto = productoRepository.findById(deliveryData.getId()).get();
+        producto.setExistencias(producto.getExistencias() - 1);
+        productoRepository.save(producto);
+
+        // Redireccion a delivery
         integracionDeliveryService.enviarPedido(deliveryData);
 
         return ResponseEntity.ok("Pedido recibido y reenviado correctamente");
